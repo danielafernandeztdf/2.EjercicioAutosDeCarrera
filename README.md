419f4a8fa197aecfc96eb5210ede9d9f354a4c6a
danielafernandeztdf <104040499+danielafernandeztdf@users.noreply.github.com>
2022-05-13 22:41:45 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# EjercicioAutosDeCarrera
+# AutosDeCarrera
 Created with CodeSandbox
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+//completar 4 vueltas seguidas 
+//programa que permita ingresar 
+//por teclado el tiempo de cada vuelta
+//El programa debe retornar el tiempo total y el
+//promedio de vuelta
+
+let vuelta1: number= 20; 
+let vuelta2: number= 15; 
+let vuelta3: number= 10; 
+let vuelta4: number= 26; 
+
+let tiempoTotal= vuelta1 + vuelta2 + vuelta3 + vuelta4; 
+console.log ("Tiempo total;", tiempoTotal);
+console.log ("Promedio de vuelta:" , tiempoTotal/4);
